6db72dcdcb35031c2ab1dd4f9c5606d9155fe2b7
Ian Sowinski <sowinj@gmail.com>
2016-10-14 14:54:23 +0200

    aaaa

--- kina.py
+++ kina.py
@@ -4,52 +4,20 @@ from bs4 import BeautifulSoup
 
 
 def assigner (arg):
-    return BeautifulSoup(urllib.request.urlopen(arg), "html.parser")
+    return BeautifulSoup(urllib.request.urlopen("http://www.filmweb.pl/showtimes/Pozna%C5%84/" + arg[0]), "html.parser")
 
 # Multikina Malta
-def multikino(adressUrl):
+def seanse(adressUrl):
     kino = assigner(adressUrl)
-    kinoDiv = kino.find("ul", "list image-list")
+    kinoDiv = kino.find("ul", "cinema-films")
     array = []
     for child in kinoDiv.children:
+        timeArray = []
+        title = child.find("a", "filmTitle").contents[0]
+        for item in child.find("ul", "hoursList").find_all("li"):
+            timeArray.append(item.contents[0])
         arrayTemp = []
-        if child == "\n":
-            pass
-        else:
-            childTitle = child.find("a", "title").contents[0]
-            arrayTemp.append(childTitle + " / ")
-            for childTime in child.find("div", "showings").children:
-                if childTime != "\n":
-                    arrayTemp.append(childTime.contents[0].strip() + " ")
-            array.append("".join(arrayTemp))
+        arrayTemp.append(title)
+        arrayTemp.append(" ".join(timeArray))
+        array.append(" / ".join(arrayTemp))
     return array
-
-# RIALTO
-def rialto(adressUrl):
-    kino = assigner(adressUrl)
-    kinoDiv = kino.find("div", "mk-accordion mk-shortcode accordion-action fancy-style ").find("ul")
-    array = []
-    for child in kinoDiv.children:
-        array.append((child.contents[0].contents[0][6:].title() + " / " + child.contents[0].contents[0][0:5]))
-    return array
-
-# MUZA
-def muza(urlAdress):
-    kino = assigner(urlAdress)
-    kinoDiv = kino.find("div", "poster-list")
-    array = []
-    for child in kinoDiv.children:
-        childTime = child.find("time")
-        if childTime == -1:
-            pass
-        else:
-            childTiltle = child.find("h4")
-            array.append((childTiltle.contents[0].contents[0].title() + " / " + childTime.contents[0]))
-    return array
-
-def dateSetter():
-    if hourNum() == "18":
-        now = datetime.date.fromordinal(datetime.date.today().toordinal()-1).strftime('%Y%m%d')
-    else:
-        now = datetime.datetime.today().strftime('%Y%m%d')
-    return now
--- poznan_bot.py
+++ poznan_bot.py
@@ -5,46 +5,24 @@ import logging
 from bs4 import BeautifulSoup
 import galeria
 import kina
-
-
-
-rialtoUrl = "http://www.kinorialto.poznan.pl/"
-muzaUrl = "http://kinomuza.pl/repertuar"
-maltaUrl = "https://multikino.pl/pl/repertuar/poznan-malta"
-browarUrl = "https://multikino.pl/pl/repertuar/poznan-stary-browar"
-piecJedenUrl = "https://multikino.pl/pl/repertuar/poznan-multikino-51"
-arsenalUrl = "http://www.arsenal.art.pl/wystawa/"
-
-# zamekUrl = "http://www.zamek.poznan.pl/news,pl,5177.html"
-# charlieUrl = "http://www.kinomalta.pl/"
-
-
-# Passing today's date to url
-
-def hourNum():
-    hourNow = int(datetime.datetime.today().strftime('%H'))
-
-    if hourNow >= 7 and hourNow < 13:
-        return "00"
-    elif hourNow >= 13 and hourNow < 19:
-        return "06"
-    elif hourNow >= 19 or (hourNow  >= 0 and hourNow < 1):
-        return "12"
-    elif hourNow >= 1 and hourNow < 7:
-        return "18"
-    else:
-        return "00"
-
-def urlMaker():
-    urlAdress = "http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=" + dateSetter() + hourNum() + "&row=400&col=180&lang=pl"
-    return urlAdress
-
-def nameSetter():
-    return "pogoda.jpg" #+ dateSetter() + hourNum() + ".jpg"
-# BOT
-
+import pogoda
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+
+
+browarUrl = ["Multikino+Stary+Browar-633", "Multikino Stary Browar"]
+apolloUrl = ["Apollo-70", "Kino Apollo"]
+bulgarska19Url = ["Bułgarska+19-1618", "Kino Bułgarska 19"]
+charlieUrl = ["Charlie+Monroe+Kino+Malta-1499", "Kino Charlie Monroe"]
+kinepolisUrl = ["Cinema+City+Kinepolis-624", "Cinema City Kinepolis"]
+plazaUrl = ["Cinema+City+Plaza-568", "Cinema City Plaza"]
+multikino51Url = ["Multikino+51-203", "Multikino 51"]
+multikinoMaltaUrl = ["Multikino+Malta-1434","Multikino Malta"]
+multikinoBrowarUrl = ["Multikino+Stary+Browar-633", "Multikino Stary Browar"]
+muzaUrl= ["Muza-75", "Kino Muza"]
+palacoweUrl = ["Nowe+Kino+Pałacowe-1854", "Nowe Kino Pałacowe"]
+rialtoUrl = ["Rialto-78", "Kino Rialto"]
+
 
 updater = Updater(token='261419062:AAFe2GkE3xUgDf3ZdMu7qmgCf9CLOTWgg6E')
 
@@ -62,35 +40,35 @@ updater.start_polling()
 
 def kino_muza (bot, update):
     global muzaUrl
-    bot.sendMessage(chat_id = update.message.chat_id, text = "KINO MUZA:\n\n" +"\n".join(kina.muza(muzaUrl)))
+    bot.sendMessage(chat_id = update.message.chat_id, text = muzaUrl[1] + "\n\n" +"\n".join(kina.seanse(muzaUrl)))
 
 kino_muza_handler = CommandHandler('kino_muza', kino_muza)
 dispatcher.add_handler(kino_muza_handler)
 
 def kino_rialto (bot, update):
     global rialtoUrl
-    bot.sendMessage(chat_id = update.message.chat_id, text = "KINO RIALTO:\n\n" +"\n".join(kina.rialto(rialtoUrl)))
+    bot.sendMessage(chat_id = update.message.chat_id, text = rialtoUrl[1] + "\n\n" +"\n".join(kina.seanse(rialtoUrl)))
 
 kino_rialto_handler = CommandHandler('kino_rialto', kino_rialto)
 dispatcher.add_handler(kino_rialto_handler)
 
 def kino_51 (bot, update):
-    global piecJedenUrl
-    bot.sendMessage(chat_id = update.message.chat_id, text ="MULTIKINO 51:\n\n" + "\n".join(kina.multikino(piecJedenUrl)))
+    global multikino51Url
+    bot.sendMessage(chat_id = update.message.chat_id, text = multikino51Url[1] + "\n\n" + "\n".join(kina.seanse(multikino51Url)))
 
 kino_51_handler = CommandHandler('kino_51', kino_51)
 dispatcher.add_handler(kino_51_handler)
 
 def kino_browar (bot, update):
-    global browarUrl
-    bot.sendMessage(chat_id = update.message.chat_id, text ="MULTIKINO BROWAR:\n\n" + "\n".join(kina.multikino(browarUrl)))
+    global multikinoBrowarUrl
+    bot.sendMessage(chat_id = update.message.chat_id, text = multikinoBrowarUrl[1] + "\n\n" + "\n".join(kina.seanse(multikinoBrowarUrl)))
 
 kino_browar_handler = CommandHandler('kino_browar', kino_browar)
 dispatcher.add_handler(kino_browar_handler)
 
 def kino_malta (bot, update):
-    global maltaUrl
-    bot.sendMessage(chat_id = update.message.chat_id, text = "MULTIKINO MALTA:\n\n" + "\n".join(kina.multikino(maltaUrl)))
+    global multikinoMaltaUrl
+    bot.sendMessage(chat_id = update.message.chat_id, text = multikinoMaltaUrl[1] + "\n\n" + "\n".join(kina.seanse(multikinoMaltaUrl)))
 
 kino_malta_handler = CommandHandler('kino_malta', kino_malta)
 dispatcher.add_handler(kino_malta_handler)
@@ -123,38 +101,3 @@ def kino (bot, update):
 
 kino_handler = CommandHandler('kino', kino)
 dispatcher.add_handler(kino_handler)
-
-
-# Main function
-
-# array = file.write("# Kino\n\ncreated at " + datetime.datetime.today().strftime('%H:%M:%S / %d.%m.%Y') + "\n\n## Muza\n\n")
-#
-#     (element + "\n")
-# file.write("\n\n## Rialto\n\n")
-# for element in rialto(rialtoUrl):
-#     file.write(element + "\n")
-# file.write("\n\n## Multikino 51\n\n")
-# for element in multikino(piecJedenUrl):
-#     file.write(element + "\n")
-# file.write("\n\n## Multikino Stary Browar\n\n")
-# for element in multikino(browarUrl):
-#     file.write(element + "\n")
-# file.write("\n\n## Multikino Malta\n\n")
-# for element in multikino(maltaUrl):
-#     file.write(element + "\n")
-# file.write("\n# Pogoda\n\n![](pogoda.jpg)")
-
-# Charlie monroe
-#
-# charlie = assigner(charlieUrl)
-# charlieDiv = charlie.find("table", "border-collapse: collapse; width: 563px; height: 342px;")
-# print("=================")
-# print("|Charlie Monroe:|")
-# print("=================")
-# for child in muzaDiv.children:
-#     childTime = child.find("time")
-#     if childTime == -1:
-#         pass
-#     else:
-#         childTiltle = child.find("h4")
-#         print(childTiltle.contents[0].contents[0], " ||| ", childTime.contents[0])
