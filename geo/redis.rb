1519e0086559d114f99d23dad3be82239bd6bbe8
Ian Sowinski <sowinj@gmail.com>
2018-03-08 16:13:11 +0100

    removed temp files

--- geo/redis.rb
+++
@@ -1,9 +0,0 @@
-require "redis"
-
-redis = Redis.new()
-
-loop do
-  redis.lpush('locations', '{"Id":"teqew","Lat":54.065045,"Lng":22.318790}')
-  puts redis.get('teqew')
-  # redis.del('teqew')
-end
