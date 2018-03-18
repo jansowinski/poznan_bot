50903af400ac109106530dfde131cdc1e4f0033c
jmajchrzak <jakub.ma@gmail.com>
2018-03-18 21:58:24 +0100

    Add to poznan bot function about public bikes in Poznan

--- rowery/rowery.py
+++ rowery/rowery.py
@@ -1,4 +1,6 @@
 import redis
+import json
+import prm_data_json as prm
 
 client = redis.StrictRedis(host='redis', port=6379, db=0)
 
@@ -6,4 +8,8 @@ while True:
     location_to_process = client.rpop('rowery')
     if location_to_process != None:
         message = json.loads(location_to_process.decode("utf-8"))
-        client.set(message['id'], '123')
+        data = prm.get_closest_stations([message['lat'], message['lng']], 5)
+        response = 'Najbliższe stacje od Ciebie: ' + '\n'
+        for item in data:
+            response = str(response) + str(item[0]) + ' - liczba dostępncyh rowerów: ' + str(item[1]) + '\n'
+        client.set(message['id'], response)
