878780e8c04483a6628c856b44749be18839fbd5
jmajchrzak <jakub.ma@gmail.com>
2018-03-18 22:27:52 +0100

    Change in laylaout of end message

--- rowery/rowery.py
+++ rowery/rowery.py
@@ -9,7 +9,7 @@ while True:
     if location_to_process != None:
         message = json.loads(location_to_process.decode("utf-8"))
         data = prm.get_closest_stations([message['lat'], message['lng']], 5)
-        response = 'Najbliższe stacje od Ciebie: ' + '\n'
+        response = '*Najbliższe stacje od Ciebie:*  \n\n'
         for item in data:
             response = str(response) + str(item[0]) + ' - liczba dostępncyh rowerów: ' + str(item[1]) + '\n'
         client.set(message['id'], response)
