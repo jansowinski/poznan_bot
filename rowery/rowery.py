62e24869f35d1243845c19471d7314e78fdffbf7
jmajchrzak <jakub.ma@gmail.com>
2018-05-27 16:22:40 +0200

    add information about free racks

--- rowery/rowery.py
+++ rowery/rowery.py
@@ -15,12 +15,19 @@ while True:
 
         for item in data:
             if item['bikes'] == '1':
-                grammar_message = ' rower'
+                grammar_message_bike = ' rower'
             elif item['bikes'] in cond:
-                grammar_message = ' rowery'
+                grammar_message_bike = ' rowery'
             else:
-                grammar_message = ' rowerów'
+                grammar_message_bike = ' rowerów'
 
-            response = str(response) + str(item['label']) + ' (' + str(item['distance']) + ' m)' + ' : ' + str(item['bikes']) + grammar_message + '\n'
+            if item['free_racks'] == '1':
+                grammar_message_racks = ' wolny zamek'
+            elif item['free_racks'] in cond:
+                grammar_message_racks = ' wolne zamki'
+            else:
+                grammar_message_racks = ' wolnych zamków'
+
+            response = str(response) + str(item['label']) + ' - ' + str(item['distance']) + ' m' + ':' + '\n   ' + str(item['bikes']) + grammar_message_bike + ' _(' + str(item['free_racks']) + '/' + str(item['bike_racks']) + grammar_message_racks + ')_' + '\n'
 
         client.set(message['id'], response)
