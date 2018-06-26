f1cc7e9bfb75106659a5d0717143433b8705fb69
jmajchrzak <jakub.ma@gmail.com>
2018-06-26 20:16:13 +0200

    handlig with error in bike module

--- rowery/rowery.py
+++ rowery/rowery.py
@@ -9,25 +9,27 @@ while True:
     if location_to_process != None:
         message = json.loads(location_to_process.decode("utf-8"))
         data = prm.stations_from_coords('Poznan', [message['lat'], message['lng']], 5)
+        try:
+            response = '🚲 *Najbliższe stacje od Ciebie o* ' + str(data[0]['time']) +'* :*\n\n'
+            cond = ['2', '3', '4']
 
-        response = '🚲 *Najbliższe stacje od Ciebie o* ' + str(data[0]['time']) +'* :*\n\n'
-        cond = ['2', '3', '4']
+            for item in data:
+                if item['bikes'] == '1':
+                    grammar_message_bike = ' rower'
+                elif item['bikes'] in cond:
+                    grammar_message_bike = ' rowery'
+                else:
+                    grammar_message_bike = ' rowerów'
 
-        for item in data:
-            if item['bikes'] == '1':
-                grammar_message_bike = ' rower'
-            elif item['bikes'] in cond:
-                grammar_message_bike = ' rowery'
-            else:
-                grammar_message_bike = ' rowerów'
+                if item['free_racks'] == '1':
+                    grammar_message_racks = ' miejsce'
+                elif item['free_racks'] in cond:
+                    grammar_message_racks = ' miejsca'
+                else:
+                    grammar_message_racks = ' miejsc'
 
-            if item['free_racks'] == '1':
-                grammar_message_racks = ' miejsce'
-            elif item['free_racks'] in cond:
-                grammar_message_racks = ' miejsca'
-            else:
-                grammar_message_racks = ' miejsc'
-
-            response = str(response) + '*' + str(item['label']) + '* - ' + str(item['distance']) + ' m' + ':' + '\n    ' + str(item['bikes']) + grammar_message_bike + '\n    ' +str(item['free_racks']) + '/' + str(item['bike_racks']) + grammar_message_racks + '\n'
+                response = str(response) + '*' + str(item['label']) + '* - ' + str(item['distance']) + ' m' + ':' + '\n    ' + str(item['bikes']) + grammar_message_bike + '\n    ' +str(item['free_racks']) + '/' + str(item['bike_racks']) + grammar_message_racks + '\n'
+        except:
+            respones = 'Błąd podczas pobierania danych 😟'
 
         client.set(message['id'], response)
